71e58000a84ff231c3256eeb099064769e2dd1fa
Eric Loots <eric.loots@gmail.com>
2020-05-27 17:47:00 +0200

    Fix inconsistent use of def versus val (#31)

--- exercises/exercise_006_using_and_summon/src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessor.scala
+++ exercises/exercise_006_using_and_summon/src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessor.scala
@@ -35,7 +35,7 @@ object SudokuDetailProcessor {
     def processorName(id: Int): String
   }
 
-  implicit def rowUpdateSender: UpdateSender[Row] = new UpdateSender[Row] {
+  implicit val rowUpdateSender: UpdateSender[Row] = new UpdateSender[Row] {
     override def sendUpdate(id: Int, cellUpdates: CellUpdates)(using sender: ActorRef[Response]): Unit = {
       sender ! RowUpdate(id, cellUpdates)
     }
