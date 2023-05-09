fd13d3ec96edb66437aa39e09d170e34eafc2015
Eric Loots <eric.loots@gmail.com>
2023-05-09 12:45:59 +0200

    Remove alternative exercise series that uses Curly Braces syntax

--- exercises/exercise_106_using_and_summon/src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailType.scala
+++
@@ -1,6 +0,0 @@
-package org.lunatechlabs.dotty.sudoku
-
-sealed trait SudokuDetailType
-case class Row(id: Int) extends SudokuDetailType
-case class Column(id: Int) extends SudokuDetailType
-case class Block(id: Int) extends SudokuDetailType
