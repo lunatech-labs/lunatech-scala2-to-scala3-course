af2fcb748813c6f866d0324434942b77747a94ea
Eric Loots <eric.loots@gmail.com>
2023-09-07 14:13:25 +0200

    Formatting exercise 20

--- exercises/exercise_020_opaque_type_aliases_alt/src/test/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessorSuite.scala
+++ exercises/exercise_020_opaque_type_aliases_alt/src/test/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessorSuite.scala
@@ -11,94 +11,90 @@ class SudokuDetailProcessorSuite extends munit.FunSuite with SudokuTestHelpers:
   override def afterAll(): Unit =
     testKit.shutdownTestKit()
 
-  test("Sending no updates to a sudoku detail processor should result in sending a SudokuDetailUnchanged messsage") {
+  test("Sending no updates to a sudoku detail processor should result in sending a SudokuDetailUnchanged messsage"):
 
-    val probe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
-    val detailProcessor = testKit.spawn(SudokuDetailProcessor[Row](id = 0))
-    detailProcessor ! Update(cellUpdatesEmpty, probe.ref)
-    probe.expectMessage(SudokuDetailUnchanged)
-  }
-
-  test(
-    "Sending an update to a fresh instance of the SudokuDetailProcessor that sets one cell to a single value should result in sending an update that reflects this update") {
-    val probe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
-    val detailProcessor = testKit.spawn(SudokuDetailProcessor[Row](id = 0))
-    detailProcessor ! Update(CellUpdates((4, Set(7))), probe.ref)
-
-    val expectedState1 =
-      SudokuDetailProcessor.RowUpdate(
-        0,
-        stringToIndexedUpdate(
-          Vector(
-            "123456 89",
-            "123456 89",
-            "123456 89",
-            "123456 89",
-            "      7  ",
-            "123456 89",
-            "123456 89",
-            "123456 89",
-            "123456 89")))
-
-    probe.expectMessage(expectedState1)
-  }
+      val probe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
+      val detailProcessor = testKit.spawn(SudokuDetailProcessor[Row](id = 0))
+      detailProcessor ! Update(cellUpdatesEmpty, probe.ref)
+      probe.expectMessage(SudokuDetailUnchanged)
 
   test(
-    "Sending a series of subsequent Updates to a SudokuDetailProcessor should result in sending updates and ultimately return no changes") {
-    val detailParentProbe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
-    val detailProcessor = testKit.spawn(SudokuDetailProcessor[Block](id = 2))
+    "Sending an update to a fresh instance of the SudokuDetailProcessor that sets one cell to a single value should result in sending an update that reflects this update"):
+      val probe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
+      val detailProcessor = testKit.spawn(SudokuDetailProcessor[Row](id = 0))
+      detailProcessor ! Update(CellUpdates((4, Set(7))), probe.ref)
 
-    val update1 =
-      val cellUpdates =
-        stringToReductionSet(
-          Vector(
-            "12345678 ",
-            "1        ", // 1: Isolated & complete
-            "   4     ", // 4: Isolated & complete
-            "12 45678 ",
-            "      78 ", // (7,8): Isolated & complete
-            "       89",
-            "      78 ", // (7,8): Isolated & complete
-            "     6789",
-            " 23   78 ")).zipWithIndex.map { _.swap }
-      Update(CellUpdates(cellUpdates*), detailParentProbe.ref)
+      val expectedState1 =
+        SudokuDetailProcessor.RowUpdate(
+          0,
+          stringToIndexedUpdate(
+            Vector(
+              "123456 89",
+              "123456 89",
+              "123456 89",
+              "123456 89",
+              "      7  ",
+              "123456 89",
+              "123456 89",
+              "123456 89",
+              "123456 89")))
 
-    detailProcessor ! update1
+      probe.expectMessage(expectedState1)
 
-    val reducedUpdate1 =
-      val cellUpdates =
-        stringToReductionSet(
-          Vector(
-            " 23 56   ",
-            "1        ",
-            "   4     ",
-            " 2  56   ",
-            "      78 ",
-            "        9",
-            "      78 ",
-            "     6  9",
-            " 23      ")).zipWithIndex.map(_.swap)
+  test(
+    "Sending a series of subsequent Updates to a SudokuDetailProcessor should result in sending updates and ultimately return no changes"):
+      val detailParentProbe = testKit.createTestProbe[SudokuDetailProcessor.Response]()
+      val detailProcessor = testKit.spawn(SudokuDetailProcessor[Block](id = 2))
 
-      BlockUpdate(2, CellUpdates(cellUpdates*))
+      val update1 =
+        val cellUpdates =
+          stringToReductionSet(
+            Vector(
+              "12345678 ",
+              "1        ", // 1: Isolated & complete
+              "   4     ", // 4: Isolated & complete
+              "12 45678 ",
+              "      78 ", // (7,8): Isolated & complete
+              "       89",
+              "      78 ", // (7,8): Isolated & complete
+              "     6789",
+              " 23   78 ")).zipWithIndex.map { _.swap }
+        Update(CellUpdates(cellUpdates*), detailParentProbe.ref)
 
-    detailParentProbe.expectMessage(reducedUpdate1)
+      detailProcessor ! update1
 
-    detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
+      val reducedUpdate1 =
+        val cellUpdates =
+          stringToReductionSet(
+            Vector(
+              " 23 56   ",
+              "1        ",
+              "   4     ",
+              " 2  56   ",
+              "      78 ",
+              "        9",
+              "      78 ",
+              "     6  9",
+              " 23      ")).zipWithIndex.map(_.swap)
 
-    val reducedUpdate2 =
-      BlockUpdate(2, stringToIndexedUpdate(Vector("", "", "", "", "", "", "", "     6   ", "")))
+        BlockUpdate(2, CellUpdates(cellUpdates*))
 
-    detailParentProbe.expectMessage(reducedUpdate2)
+      detailParentProbe.expectMessage(reducedUpdate1)
 
-    detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
+      detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
 
-    val reducedUpdate3 =
-      BlockUpdate(2, stringToIndexedUpdate(Vector(" 23 5    ", "", "", " 2  5    ", "", "", "", "", "")))
+      val reducedUpdate2 =
+        BlockUpdate(2, stringToIndexedUpdate(Vector("", "", "", "", "", "", "", "     6   ", "")))
 
-    detailParentProbe.expectMessage(reducedUpdate3)
+      detailParentProbe.expectMessage(reducedUpdate2)
 
-    detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
+      detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
 
-    detailParentProbe.expectMessage(SudokuDetailUnchanged)
+      val reducedUpdate3 =
+        BlockUpdate(2, stringToIndexedUpdate(Vector(" 23 5    ", "", "", " 2  5    ", "", "", "", "", "")))
 
-  }
+      detailParentProbe.expectMessage(reducedUpdate3)
+
+      detailProcessor ! Update(cellUpdatesEmpty, detailParentProbe.ref)
+
+      detailParentProbe.expectMessage(SudokuDetailUnchanged)
